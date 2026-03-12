9cd3ff5dd7df39f65b0e63264be992e11702df8f
Greg Coladarci <coladarci@gmail.com>
2026-03-11 19:31:09 -0700

    fix: never show homescreen if logged in

--- test/phoenix_starter_kit_web/controllers/page_controller_test.exs
+++ test/phoenix_starter_kit_web/controllers/page_controller_test.exs
@@ -1,8 +1,22 @@
 defmodule PhoenixStarterKitWeb.PageControllerTest do
   use PhoenixStarterKitWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Install App"
+  import PhoenixStarterKit.PartnersFixtures
+
+  describe "GET /" do
+    test "renders home page when not logged in", %{conn: conn} do
+      conn = get(conn, ~p"/")
+      assert html_response(conn, 200) =~ "Install App"
+    end
+
+    test "redirects to settings when logged in", %{conn: conn} do
+      partner = partner_fixture()
+      partner_user = partner_user_fixture()
+      {:ok, _} = PhoenixStarterKit.Partners.connect_partner_user(partner, partner_user)
+      conn = log_in_partner_user(conn, partner_user, partner.id)
+
+      conn = get(conn, ~p"/")
+      assert redirected_to(conn) == ~p"/settings"
+    end
   end
 end
